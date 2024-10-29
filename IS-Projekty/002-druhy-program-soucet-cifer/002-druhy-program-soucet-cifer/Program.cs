f6f5926a2f89c335a6616a8a1c67db2974f4bdf6
Matej Čižmár <matej.cizmar@tul.cz>
2024-10-29 18:23:23 +0100

    #002 - oprava chyb

--- IS-Projekty/002-druhy-program-soucet-cifer/002-druhy-program-soucet-cifer/Program.cs
+++ IS-Projekty/002-druhy-program-soucet-cifer/002-druhy-program-soucet-cifer/Program.cs
@@ -5,7 +5,7 @@ using Microsoft.Win32.SafeHandles;
 class Program
 {
     static void Main() {
-        Console.Write("Zadejte diferenci (cele cislo): ");
+        Console.Write("Zadejte cislo (cele cislo): ");
         int number;  
         while(!int.TryParse(Console.ReadLine(), out number)){ // vykricni je negace, dokud nezada uzivatel nesplni podminku, program se nespusti ale ani nespadne
         Console.Write("Prvni cislo musi byt celociselne");
@@ -19,13 +19,13 @@ class Program
             digit = number % 10;
             number = (number - digit)/10;
             Console.WriteLine("Digit = {0}", digit);
-            suma = suma + digit;
-            multi = multi * digit;
+            suma = number + digit;
+            multi = number * digit;
         }
 
         Console.WriteLine();
         Console.WriteLine("Soucet cifer cisla: {0} je {1}", numberBackup, suma);
-        Console.WriteLine("Soucet cifer cisla: {0} je {1}", numberBackup, multi);
+        Console.WriteLine("Soucin cifer cisla: {0} je {1}", numberBackup, multi);
     
 
         }
